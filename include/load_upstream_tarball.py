ee40f4f5ab1c5164dd6d13cfa35c38dac693f837
Colin Gilbert <colingilbert86@gmail.com>
2024-12-18 14:58:00 -0600

    Fix typo

--- include/load_upstream_tarball.py
+++ include/load_upstream_tarball.py
@@ -6,7 +6,7 @@ from .gentoomuch_common import gentoo_upstream_url, gentoo_signing_key, stages_p
 from .verify_tarball import verify_tarball
 from .containerize import containerize
 
-def load_tarball(arch, profile, path):
+def load_upstream_tarball(arch, profile, path):
     fname = os.path.split(path)[1]
     new_tarball_path = os.path.join(stages_path, fname)
     code = os.system("cp " + path + " " + new_tarball_path)
@@ -16,4 +16,4 @@ def load_tarball(arch, profile, path):
     if code == 0:
         pass
     print("Containerizing upstream tarball")
-    return containerize(path, arch, profile, '', bool(True))
+    return containerize(path, arch, profile, stage_define = '', upstream = True)
