de178ee658724ac50f1130eedbfcd7f6f8601145
Colin Gilbert <colingilbert86@gmail.com>
2024-12-18 14:00:17 -0600

    Cleanup

--- include/save_tarball.py
+++ include/save_tarball.py
@@ -18,8 +18,7 @@ from .package_from_patch import package_from_patch
 from .build_kernel import build_kernel
 from .sign_stage import sign_stage
 
-def save_tarball(arch: str, profile: str, stage_define: str, upstream: bool, patches: [str] = [], patches_have_been_compiled: bool = True, kconfig: str = '', emerge_kernel: bool = False, friendly_name = ''):
-
+def save_tarball(arch: str, profile: str, stage_define: str, upstream: bool, patches: [str] = [], patches_have_been_compiled: bool = True, kconfig: str = '', emerge_kernel: bool = False, strip_deps = False, friendly_name = ''):
     if friendly_name != '':
         archive_name = friendly_name
     else:
@@ -49,41 +48,16 @@ def save_tarball(arch: str, profile: str, stage_define: str, upstream: bool, pat
     print('PACKAGES TO INSTALL : ' + packages_str)
     if kconfig:
         print("KCONFIG: " + kconfig)
-      # The following dogs' meal of a command preps a stage inside a docker container. It then changes root into it and emerges. Then, it exits the chroot, unmounts all temporaries, and packs a tarball as "stage3-<arch>-<base>-<user-stage-define>.tar.gz"
     cmd_str = "cd " + output_path + " && "
     cmd_str += "docker-compose run gentoomuch-builder /bin/bash -c \""
     cmd_str += "emerge --root=/ pigz && "
-    #cmd_str += "mkdir -p /mnt/gentoo/etc/portage &&"
     cmd_str += "rm -rf /mnt/gentoo/* && "
     cmd_str += "cd /mnt/gentoo && "
-    # cmd_str += "echo EXTRACTING STAGE3 ARCHIVE && "
-    # cmd_str += "tar xpvf /stage3-* --xattrs-include='*.*' --numeric-owner && "
     cmd_str += "tar xpf /stage3-* --numeric-owner && "
     cmd_str += "rm -rf /mnt/gentoo/etc/portage/* && "
     cmd_str += "rsync -aXH /etc/portage/* /mnt/gentoo/etc/portage/ && "
-    #cmd_str += "mount -t proc none /mnt/gentoo/proc && "
-    #cmd_str += "mount -t tmpfs none /mnt/gentoo/tmp && "
-    #cmd_str += "mount --rbind /sys /mnt/gentoo/sys && "
-    #cmd_str += "mount --make-rslave /mnt/gentoo/sys && "
-    #cmd_str += "mount -o bind /dev /mnt/gentoo/dev && "
-    #cmd_str += "mount --rbind /dev /mnt/gentoo/dev && "
-    #cmd_str += "mount --make-rslave /mnt/gentoo/dev && "
-    #cmd_str += "mount -t tmpfs none /mnt/gentoo/var/tmp && "
-    #cmd_str += "mkdir -p /mnt/gentoo/var/tmp/portage && "
-    #cmd_str += "mount --bind /var/tmp/portage /mnt/gentoo/var/tmp/portage && "
-    #cmd_str += "mount --bind /var/cache/binpkgs /mnt/gentoo/var/cache/binpkgs && "
-    #cmd_str += "mount --bind /usr/src /mnt/gentoo/usr/src && "
-    #cmd_str += "mkdir -p /mnt/gentoo/mnt/ccache_portage && "
-    #cmd_str += "mount --bind /mnt/ccache_portage /mnt/gentoo/mnt/ccache_portage && "
-    #cmd_str += "mkdir -p /mnt/gentoo/var/db/repos/gentoo && "
-    #cmd_str += "mount --bind /var/db/repos/gentoo /mnt/gentoo/var/db/repos/gentoo && "
-    #cmd_str += "mkdir -p /mnt/gentoo/mnt/kconfigs && "
-    #cmd_str += "mount --bind /mnt/kconfigs /mnt/gentoo/mnt/kconfigs && "
     cmd_str += "echo 'UTC' > /etc/timezone && "
     cmd_str += "echo 'nameserver 8.8.8.8' > /etc/resolv.conf && "
-    #cmd_str += "chroot . /bin/bash -c '" # Enter chroot
-    # cmd_str += "env-update && "
-    # cmd_str += ". /etc/profile && "
     cmd_str += "emerge --with-bdeps=y -j" + jobs + (" --emptytree " if upstream else " -uD --changed-use --newuse ") + packages_str + "@world && "
     for patch in patches:
         if patch != '':
@@ -94,44 +68,28 @@ def save_tarball(arch: str, profile: str, stage_define: str, upstream: bool, pat
                 else:
                     cmd_str += "emerge --root=/mnt/gentoo -j" + jobs + " --oneshot --onlydeps =" + package + " && "
                     cmd_str += "emerge --root=/mnt/gentoo -j" + jobs + " --oneshot --usepkg n =" + package + " && "
-                    #cmd_str += "emerge --oneshot --usepkg n =" + package + " && "
     if emerge_kernel:
         cmd_str += 'rm -rf /usr/src/* && '
         cmd_str += 'emerge --sys-kernel/gentoo-sources && '
     if kconfig:
         cmd_str += "echo 'MAKING " + kconfig + "' && "
-        # results = build_kernel(arch, profile, kconfig, jobs)
-        # if not results:
-        #     print("COULD NOT BUILD KERNEL " + kconfig)
-        #     return (False,'')
         cmd_str += "cp " + os.path.join(kconfigs_mountpoint, kconfig + ".kconf") + " /usr/src/linux/.config && "
         cmd_str += "cd /usr/src/linux && "
         cmd_str += "make -j" + jobs + " && "
         cmd_str += "INSTALL_PATH=/mnt/gentoo/boot make install && "
         cmd_str += "INSTALL_MOD_PATH=/mnt/gentoo make modules_install && "
-        # cmd_str += "make clean && "
         cmd_str += 'emerge --onlydeps --root=/mnt/gentoo @module-rebuild && '
         cmd_str += 'emerge --usepkg n --root=/mnt/gentoo @module-rebuild && '
-    #cmd_str += "emerge --depclean --with-bdeps=n && " # Remove build deps
+    if strip_deps:
+        cmd_str += "emerge --depclean --with-bdeps=n && "
     cmd_str += "cd / && "
     cmd_str += "chown " + uid + ":" + gid + " -R /var/tmp/portage && "
-    #cmd_str += "' && " # Exit chroot
-    #cmd_str += "umount -fl /mnt/gentoo/tmp && "
-    #cmd_str += "umount -fl /mnt/gentoo/proc && "
-    #cmd_str += "umount -fl /mnt/gentoo/sys && "
-    #cmd_str += "umount -fl /mnt/gentoo/dev && "
-    #cmd_str += "umount -fl /mnt/gentoo/var/db/repos/gentoo && "
-    #cmd_str += "umount -fl /mnt/gentoo/var/cache/binpkgs && "
-    #cmd_str += "umount -fl /mnt/gentoo/var/tmp/portage && "
-    #cmd_str += "umount -fl /mnt/gentoo/usr/src && "
-    #cmd_str += "umount -fl /mnt/gentoo/mnt/kconfigs && "
     cmd_str += "cd /mnt/gentoo && "
     cmd_str += "echo 'SAVING STAGE INTO TAR ARCHIVE' && "
     cmd_str += "tar -cf /mnt/stages/" + archive_name + " . --use-compress-program=pigz --xattrs --selinux --numeric-owner --acls && "
     cmd_str += "chown " + uid + ":" + gid + " /mnt/stages/" + archive_name
     cmd_str += "\""
     swap_stage(arch, profile, stage_define, upstream)
-    #print(cmd_str)
     code = os.system(cmd_str)
     if not code == 0:
         print("FAILED TO CREATE TARBALL: " + archive_name)
