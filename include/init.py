ad64091043aff5f9396d5c777e7ef39c96e4d959
Colin Gilbert <colingilbert86@gmail.com>
2024-12-31 11:12:57 -0600

    Edit cleanup function

--- include/init.py
+++ include/init.py
@@ -6,7 +6,6 @@ def init():
     cmd_str += 'mkdir -p ~/gentoomuch-data/stages && '
     cmd_str += 'mkdir -p ~/gentoomuch-data/bootstrap && '
     cmd_str += 'mkdir -p ~/gentoomuch-data/squashed_ports && '
-    cmd_str += 'mkdir -p ~/gentoomuch-data/squashed_ports_host_mountpoint && '
-    cmd_str += 'touch ~/gentoomuch-data/profiles'# && '
+    cmd_str += 'mkdir -p ~/gentoomuch-data/squashed_ports_host_mountpoint'
     #cmd_str += "echo 'gentoo-sources' > ~/gentoomuch-data/kernel-sources"
     os.system(cmd_str)
